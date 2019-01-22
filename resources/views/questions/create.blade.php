6351fdec1c46d90cf9807b0ad0665963fbd717e9
Dmitriy Vepretskiy <d.vepretskiy@gmail.com>
2019-01-22 17:00:32 +0200

    Implement form validation and store the question in DB

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -21,7 +21,7 @@
                             <label for="question-title">
                                 Question title
                             </label>
-                            <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? ' is-invalid' : ''}}">
+                            <input type="text" name="title" id="question-title" value="{{ old('title') }}" class="form-control {{ $errors->has('title') ? ' is-invalid' : ''}}">
                             @if($errors->has('title'))
                                 <div class="invalid-feedback">
                                     <strong>{{ $errors->first('title') }}</strong>
@@ -32,7 +32,7 @@
                             <label for="question-body">
                                 Explain your question
                             </label>
-                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? ' is-invalid' : ''}}"></textarea>
+                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? ' is-invalid' : ''}}">{{ old('body') }}</textarea>
                             @if($errors->has('body'))
                                 <div class="invalid-feedback">
                                     <strong>{{ $errors->first('body') }}</strong>
