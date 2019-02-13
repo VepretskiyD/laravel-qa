1df190a631c7e0b9fc754f982b2191039692afbc
Dmitriy Vepretskiy <d.vepretskiy@gmail.com>
2019-02-14 00:19:13 +0200

    Implement delete functionality for answer

--- app/Answer.php
+++ app/Answer.php
@@ -28,8 +28,10 @@ class Answer extends Model
     	parent::boot();
     	static::created(function ($answer) {
     		$answer->question->increment('answers_count');
-    		$answer->question->save();
     	});
+        static::deleted(function($answer) {
+            $answer->question->decrement('answers_count');
+        });
     }
     public function getCreatedDateAttribute()
     {
