573502462f116504101090c9b838cbba58ccaf79
Dmitriy Vepretskiy <d.vepretskiy@gmail.com>
2019-02-16 17:10:10 +0200

    Implement many to many polymorphic realtionship and make the vote question controls working

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UsersQuestionAnswersTableSeeder::class,
-            FavoritesTableSeeder::class
+            FavoritesTableSeeder::class,
+            VotablesTableSeeder::class
         ])
     }
 }
